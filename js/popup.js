04124d154e7135c52d5779e845cadc73772438f5
Robert J <chew-z@users.noreply.github.com>
2018-08-11 23:40:43 +0200

    Bump to 1.7.2

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,7 @@
 /* global Fuse */
 const fuseOptions = {
-    keys: [
-        {name:"title",weight:0.7},
-        {name:"path",weight:0.2},
-        {name:"url",weight:0.1}
-    ], // 'title', 'url', 'path'],
-    threshold: 0.50,
+    keys: [{ name: "title", weight: 0.7 }, { name: "path", weight: 0.2 }, { name: "url", weight: 0.1 }], // 'title', 'url', 'path'],
+    threshold: 0.5,
     location: 0,
     distance: 160,
     includeScore: true,
@@ -24,11 +20,11 @@ function displayResults(results, location) {
      * @requires module:jquery
      */
     $(location).empty();
-    const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
-    const dateTimeFormat = new Intl.DateTimeFormat('pl-PL', dateOptions);
+    const dateOptions = { year: "numeric", month: "short", day: "numeric" };
+    const dateTimeFormat = new Intl.DateTimeFormat("pl-PL", dateOptions);
     const resultNodes = results.map((res) => {
         // console.log(res);
-        const score = res.score.toFixed(3);
+        const score = (100.0 * res.score).toFixed(3);
         const title = res.item.title;
         const path = res.item.path;
         const url = res.item.url;
@@ -38,8 +34,8 @@ function displayResults(results, location) {
             <div class="row search-item" data-toggle="tooltip" data-placement="top" title="${url}">
                 <a href="${url}" target="_blank" class="url">
                     <div class="score">${score}:&ensp;</div>
-                    <div class="path">${path}:&ensp;</div>
                     <div class="date">${bookmarkDate}</div>
+                    <div class="path">${path}:&ensp;</div>
                     <div class="title">${title}</div>
                 </a>
             </div>
@@ -58,15 +54,16 @@ function flattenArray(arr) {
      * @returns {Array.<BookmarkTreeNode>}
      */
     let tempPtr = arr;
-    while (tempPtr.some(node => node.hasOwnProperty('children'))) {
+    while (tempPtr.some((node) => node.hasOwnProperty("children"))) {
         tempPtr = tempPtr.reduce((acc, curr) => {
-            if (curr.hasOwnProperty('children')) {
+            if (curr.hasOwnProperty("children")) {
                 return acc.concat(curr.children);
             } else {
                 return acc.concat([curr]);
             }
         }, []);
     }
+
     return tempPtr;
 }
 
@@ -76,9 +73,9 @@ function initNodePaths(parent, parentPath) {
      * @param {BookmarkTreeNode} parent - The node whose path will be given to its children
      * @param {string} parentPath - The path to give to
      */
-    parent.children.forEach(child => {
+    parent.children.forEach((child) => {
         child.path = parentPath;
-        if (child.hasOwnProperty('children')) {
+        if (child.hasOwnProperty("children")) {
             child.path += `${child.title}`;
             initNodePaths(child, child.path);
         }
@@ -86,23 +83,21 @@ function initNodePaths(parent, parentPath) {
 }
 
 // initialise Fuse instance
-chrome.bookmarks.getTree(results => {
-    const bookmarks = results[0]
-        .children
-        .find(child => child.title === 'Other bookmarks');
-    initNodePaths(bookmarks, '');
+chrome.bookmarks.getTree((results) => {
+    const bookmarks = results[0].children.find((child) => child.title === "Other bookmarks");
+    initNodePaths(bookmarks, "");
     bookmarksList = flattenArray(bookmarks.children);
     fuse = new Fuse(bookmarksList, fuseOptions);
 });
 
-$('#search-input').on('input', function(event) {
+$("#search-input").on("input", function(event) {
     const query = $(this).val();
     const results = fuse.search(query);
 
     if (results.length > 0) {
-        $('body').addClass('res-present');
+        $("body").addClass("res-present");
     } else {
-        $('body').removeClass('res-present');
+        $("body").removeClass("res-present");
     }
-    displayResults(results, '.results-box');
+    displayResults(results, ".results-box");
 });
